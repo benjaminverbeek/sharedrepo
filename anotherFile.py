64354ba381d304a223fbff89a00b0206ad1dfabc
BENJAMIN VERBEEK <BENJAMINVERBEEK79@gmail.com>
2021-04-01 14:52:05 +0200

    Added revolutionaryFunction to anotherFile

--- anotherFile.py
+++ anotherFile.py
@@ -2,3 +2,7 @@ for i in range(1):
 	print("Hello World")
 
 # NOTE: some txt
+def revolutionaryFunction(x):
+	return lambda a: a**x
+
+print(f"Revolutionary function in action: {revolutionaryFunction(3)(5)}")
